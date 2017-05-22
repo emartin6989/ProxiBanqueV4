5cb4088078e0f24e5e08b2ce1896bf93debd952b
izardj <izard.jerome@free.fr>
2017-05-23 00:19:01 +0200

    Test de la DAO (le rollback ne fonctionne pas toujours d'ou les @Ignore)

--- proxiv4/dao/src/main/java/com/gtm/proxiv4/dao/ConseillerRepository.java
+++ proxiv4/dao/src/main/java/com/gtm/proxiv4/dao/ConseillerRepository.java
@@ -3,13 +3,14 @@ package com.gtm.proxiv4.dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.gtm.proxiv4.metier.Conseiller;
-import com.gtm.proxiv4.metier.Gerant;
 
-public interface ConseillerRepository extends JpaRepository<Conseiller,Long> {
+@Repository
+public interface ConseillerRepository extends JpaRepository<Conseiller, Long> {
 
 	public List<Conseiller> findByGerantId(long id);
-	
+
 	public Conseiller findOneByEmail(String email);
 }
---
+++ proxiv4/dao/src/test/java/com/gtm/proxiv4/dao/TestConseillerRepository.java
@@ -0,0 +1,95 @@
+package com.gtm.proxiv4.dao;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.gtm.proxiv4.metier.Conseiller;
+import com.gtm.proxiv4.metier.Gerant;
+ 
+@ContextConfiguration(locations = "classpath:ApplicationContext.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+public class TestConseillerRepository {
+	
+	@Autowired
+	private ConseillerRepository conseillerRepo;
+	@Autowired
+	private GerantRepository gerantRepo;
+	
+	@Test
+	@Transactional
+	public void testAjouterConseiller() {
+		Conseiller c = new Conseiller();
+		c.setNom("NomDuTest");
+		conseillerRepo.save(c);
+		
+		assertNotNull(conseillerRepo.findOne(c.getId()));
+	}
+	
+	@Test
+	@Transactional
+	public void testSupprimerConseiller() {
+		Conseiller c = new Conseiller();
+		c.setId(999);
+		c.setNom("NomTest");
+		c.setPrenom("PrenomTest");
+		conseillerRepo.save(c);
+		
+		conseillerRepo.delete(c);
+			
+		assertNull(conseillerRepo.findOne(999L));
+	}
+	
+	@Ignore
+	@Test
+	@Transactional
+	public void testFindByGerantId(){
+		List<Conseiller> conseillers = new ArrayList<Conseiller>();
+		Gerant g = new Gerant();
+		Conseiller c1 = new Conseiller();
+		Conseiller c2 = new Conseiller();
+		Conseiller c3 = new Conseiller();
+		
+		c1.setGerant(g);
+		c2.setGerant(g);
+		c3.setGerant(g);
+		
+		conseillers.add(c1);
+		conseillers.add(c2);
+		conseillers.add(c3);
+		g.setConseillers(conseillers);
+		
+		gerantRepo.save(g);
+		conseillerRepo.save(c1);
+		conseillerRepo.save(c2);
+		conseillerRepo.save(c3);
+		
+		assertEquals(3, conseillerRepo.findByGerantId(g.getId()).size());
+	}
+	
+	@Ignore
+	@Test
+	@Transactional
+	public void testFindOneByEmail(){
+		Conseiller c = new Conseiller();
+		c.setId(33L);
+		c.setNom("TEST");
+		c.setEmail("email@test.fr");
+		conseillerRepo.save(c);
+		
+		assertEquals("TEST", conseillerRepo.findOneByEmail("email@test.fr").getNom());
+	}
+	
+
+}
