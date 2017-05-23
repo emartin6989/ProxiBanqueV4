41040792f5f350900644d79aae76a59a2e0e7966
emartin <emartin6989@gmail.com>
2017-05-23 10:34:38 +0200

    repository

--- proxiv4/dao/src/main/java/com/gtm/proxiv4/dao/CompteRepository.java
+++ proxiv4/dao/src/main/java/com/gtm/proxiv4/dao/CompteRepository.java
@@ -1,9 +1,12 @@
 package com.gtm.proxiv4.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.gtm.proxiv4.metier.Compte;
 
 public interface CompteRepository extends JpaRepository<Compte,Long>  {
 
+	public List<Compte> findByClientId(long id);
 }
