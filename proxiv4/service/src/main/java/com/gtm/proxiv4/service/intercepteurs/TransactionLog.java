1208fa374da0673d44af7e1ea151081446bae4f5
emartin <emartin6989@gmail.com>
2017-05-25 20:28:31 +0200

    aspect nettoyé

--- proxiv4/service/src/main/java/com/gtm/proxiv4/service/intercepteurs/TransactionLog.java
+++ proxiv4/service/src/main/java/com/gtm/proxiv4/service/intercepteurs/TransactionLog.java
@@ -2,9 +2,7 @@ package com.gtm.proxiv4.service.intercepteurs;
 
 import java.util.Date;
 
-import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,27 +16,16 @@ import com.gtm.proxiv4.metier.Transaction;
 @Aspect
 public class TransactionLog {
 
-
-	//@Autowired
-	//private Transaction transaction;
-
 	@Autowired
 	private TransactionRepository transactionRepo;
-	
-	
-
-
 
 	@Pointcut("execution(* *.effectuerVirement(..)) && args(compteDebiteur, compteCrediteur, montant)")
 	public void auditer(Compte compteDebiteur, Compte compteCrediteur, double montant) {
 	}
 
 	@AfterReturning("auditer(compteDebiteur, compteCrediteur, montant)")
-	public void Enregistrement(Compte compteDebiteur, Compte compteCrediteur, double montant)
-			throws Throwable {
-		
-		
-		;
+	public void Enregistrement(Compte compteDebiteur, Compte compteCrediteur, double montant) throws Throwable {
+
 		Transaction transaction = new Transaction();
 		transaction.setDate(new Date());
 		transaction.setCompteCrediteur(compteCrediteur);
